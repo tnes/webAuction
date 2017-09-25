527393bffab6850ed665fc6e552bee3e34256302
VidarUre <vidarure@gmail.com>
2017-09-25 21:50:07 +0200

    Diverse småpjåt

--- WebAuction/WebAuction-war/src/java/no/hvl/dat250/User.java
+++ WebAuction/WebAuction-war/src/java/no/hvl/dat250/User.java
@@ -5,6 +5,7 @@
  */
 package no.hvl.dat250;
 
+import java.util.List;
 import javax.ejb.Stateful;
 import javax.ejb.LocalBean;
 
@@ -15,7 +16,66 @@ import javax.ejb.LocalBean;
 @Stateful
 @LocalBean
 public class User {
+    
+    private int id;
+    
+    private double rating;
+    //private ProductCatalog catalog;
+    //private List<Feedback> feedback;
+    
+    private String username;
+    private String password;
+    private boolean loggedIn;
+    
+    //List<Product> acquiredProducts;
+    //List<Product> soldProducts;
+    
+    public User() {
+        
+    }
+    
+    public int getId() {
+        return id;
+    }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    
+    public String logIn() {
+        return ""; // "SUCCESS" or "FAIL"
+    }
+    
 }
