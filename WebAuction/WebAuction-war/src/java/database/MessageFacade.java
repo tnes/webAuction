4ed8fd872083538d1ad45cc884671f9bf6406209
tnes <torkel.nes@gmail.com>
2017-09-27 14:27:36 +0200

    abstract

---
+++ WebAuction/WebAuction-war/src/java/database/MessageFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import javax.ejb.Stateless;
+import javax.jms.Message;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author TorkelNes
+ */
+@Stateless
+public class MessageFacade extends AbstractFacade<Message> {
+    @PersistenceContext(unitName = "WebAuction-warPU")
+    private EntityManager em;
+    
+    @Override
+    protected EntityManager getEntityManager() {
+    return em;
+    }
+    
+    public MessageFacade() {
+    super(Message.class);
+    }
+}
