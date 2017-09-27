955f6480b0515153d7363542606e6bf6dbec9009
Sidiri <sinde5@hotmail.com>
2017-09-27 08:44:13 +0200

    Jeg fikk til noe Ure ikke fikk til
    Lololol

---
+++ WebAuction/WebAuction-war/src/java/no/hvl/dat250/Feedback.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.hvl.dat250;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author S1ndr3
+ */
+@Stateless
+@LocalBean
+public class Feedback {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
